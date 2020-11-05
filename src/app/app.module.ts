05df4a79d60a605505a1a527ab760dab4387c7da
Yakami <58188693+yakamijack@users.noreply.github.com>
2020-11-05 15:14:24 -0300

    05/11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,11 +19,14 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+  
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://appbasico.web.app/cadastro',
+  firebaseConfig = {
+    apiKey: "AIzaSyBwPqVXENq1MpLIm1QXUBBAeoRtwDNNC_8",
+    authDomain: "appbasico.firebaseapp.com",
+    databaseURL: "https://appbasico.firebaseio.com",
+    projectId: "appbasico",
+    storageBucket: "appbasico.appspot.com",
+    messagingSenderId: "844488375730",
+    appId: "1:844488375730:web:c34a54e4fdc2c465b438ab"
+  };
 };
